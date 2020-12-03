83c2eecc8a3aa5792d316b55da997edf165cc2dd
sem-aa <66129330+sem-aa@users.noreply.github.com>
2020-12-04 01:06:35 +0200

    добавляет бесконечный скрол

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import "./main.css";
+
 import NewsApiService from "./apiService";
 import hitsTpl from "./templat.hbs";
 
 const searchForm = document.querySelector("#search-form");
-const loadMore = document.querySelector(".button");
+const sentinel = document.querySelector(".infScroll");
+// const loadMore = document.querySelector(".button");
 const galleryPicture = document.querySelector(".gallery");
 let quantityFetch = 0;
 
 const newsApiService = new NewsApiService();
 
 searchForm.addEventListener("submit", searchFormFn);
-loadMore.addEventListener("click", loadMoreFn);
+// loadMore.addEventListener("click", loadMoreFn);
 
 async function searchFormFn(e) {
   e.preventDefault();
@@ -19,14 +21,15 @@ async function searchFormFn(e) {
   newsApiService.query = e.currentTarget.elements.query.value;
   newsApiService.resetPage();
   await newsApiService.fetchArticles().then(appendHitsMarkup);
-  showHideBtn();
+
+  // showHideBtn();
 }
 
-async function loadMoreFn() {
-  await newsApiService.fetchArticles().then(appendHitsMarkup);
-  scrollTo(galleryPicture.lastElementChild);
-  showHideBtn();
-}
+// async function loadMoreFn() {
+//   await newsApiService.fetchArticles().then(appendHitsMarkup);
+//   scrollTo(galleryPicture.lastElementChild);
+//   showHideBtn();
+// }
 
 function appendHitsMarkup(hits) {
   galleryPicture.insertAdjacentHTML("beforeend", hitsTpl(hits));
@@ -37,20 +40,37 @@ function clearPictureContainet() {
   galleryPicture.innerHTML = "";
 }
 
-function scrollTo(element) {
-  // setTimeout(() => {
-  window.scrollTo({
-    left: 0,
-    top: element.offsetTop + document.documentElement.clientWidth,
-    behavior: "smooth",
-  });
-  // }, 1000);
-}
+// function scrollTo(element) {
 
-function showHideBtn() {
-  // setTimeout(() => {
-  if (quantityFetch < 12) {
-    loadMore.classList.add("is-hidden");
-  } else loadMore.classList.remove("is-hidden");
-  // }, 500);
-}
+//   window.scrollTo({
+//     left: 0,
+//     top: element.offsetTop + document.documentElement.clientWidth,
+//     behavior: "smooth",
+//   });
+
+// }
+
+// function showHideBtn() {
+
+//   if (quantityFetch < 12) {
+//     loadMore.classList.add("is-hidden");
+//   } else loadMore.classList.remove("is-hidden");
+
+// }
+
+// Infinity scroll
+
+const onEntry = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting && newsApiService.query !== "") {
+      console.log("грузим контент");
+      newsApiService.fetchArticles().then(appendHitsMarkup);
+    }
+  });
+};
+const options = {
+  rootMargin: "200px",
+};
+
+const observer = new IntersectionObserver(onEntry, options);
+observer.observe(sentinel);
---
+++ src/infScroll.js
@@ -0,0 +1,16 @@
+// const callback = (entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       console.log("Hi from callback forEach");
+//       console.log(entry);
+//     }
+//   });
+// };
+// const options = {
+//   rootMargin: "100px",
+//   treshold: 0.5,
+// };
+// const observer = new IntersectionObserver(callback, options);
+
+// const sentinel = document.querySelector(".infScroll");
+// observer.observe(sentinel);
